1b2e8d2c012a65de73387e6426b0df6e9c06ab74
Nita Correa <a1704505@myy.haaga-helia.fi>
2019-09-11 09:52:46 +0300

    login.php redirect

--- login.php
+++ login.php
@@ -1,1 +1,1 @@
-<?phpif($_POST['password'] =='password' && $_POST['username'] == 'username') {session_start();$_SESSION['username']='username';redirect("list.php");} else {echo 'Login failed';}function redirect($url) {ob_start();header('Location: '.$url);ob_end_flush();die();}?>
+<?phpif($_POST['password'] =='password' && $_POST['username'] == 'username') {session_start();$_SESSION['username']='username';redirect("login.php");} else {echo 'Login failed';}function redirect($url) {ob_start();header('Location: '.$url);ob_end_flush();die();}?>
